7d700515dacf8063052a236d14e6ca996520ad9c
Eric Ren <eric1423247764@gmail.com>
2023-02-24 19:04:15 -0800

    search box creates, logo deleted

--- hello-doc/src/App.js
+++ hello-doc/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component, Fragment } from 'react';
+import Search from './Back-end-stuffs/Search';
+import About from './Back-end-stuffs/About';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Navbar from './Back-end-stuffs/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    user: {},
+    loading: false,
+    alert: null,
+  };
+
+  //clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  //set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    //make it go away after 5 sec
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
+  render() {
+    const { users, user, loading } = this.state;
+    return (
+      <div className='App'>
+        <div className='container'>
+        <Navbar />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <h1>This is the Home Page!</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
