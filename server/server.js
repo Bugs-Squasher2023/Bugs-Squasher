9642c93b565b1e658214984ec2c2bac3018343f9
Charles (Ngoc) Nguyen <59405079+thinh241819@users.noreply.github.com>
2023-02-26 22:18:42 +0000

    moved the patientlogin route into separate file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const session = require("express-session");
 
-const patients = require('../client/src/database/patients.json');
-
-
+const patients = require('../halodoc/src/database/patients.json');
 const app = express();
+
 app.use(express.json())
 
 app.use(session({
@@ -15,24 +14,7 @@ app.use(session({
 
 app.get('/api', (req, res) => {res.json({msg: 'Hello World'})});
 
-app.get('/patientlogin', (req, res) => {
-    res.redirect('/patientlogin');
-});
-
-app.post('/patientlogin', async (req, res) => {
-    const { email, password } = req.body;
-    const patient = patients.find((patient) => patient.email === email && patient.password === password);
-    if (!patient) {
-        res.json(null)
-        return;
-    }
-    try {
-        req.session.patientID = patient.id;
-        res.json(patient)
-    } catch (err) {
-      console.log(err)
-    }
-  });
+app.use('/patientlogin', require('./routes/authentication'))
 
 app.post('/logout', (req, res) => {
     req.session.patientID = null; // deletes the patientID property from the session object
@@ -47,7 +29,7 @@ app.get('/dashboard', async (req, res) => {
       }
     const patient = patients.find((patient) => patient.id === req.session.patientID);
     res.json({data: patient})
-  });
+});
 
 
 
